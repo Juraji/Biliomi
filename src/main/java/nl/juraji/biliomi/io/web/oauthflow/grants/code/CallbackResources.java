334f89f5fb6125e4861a072d2e41ea53f3faf215
Juraji <robin@juraji.nl>
2017-10-13 19:58:49 +0200

    - Remove authorize.html, since it's never used

--- src/main/java/nl/juraji/biliomi/io/web/oauthflow/grants/code/CallbackResources.java
+++ src/main/java/nl/juraji/biliomi/io/web/oauthflow/grants/code/CallbackResources.java
@@ -7,19 +7,15 @@ import java.net.URL;
  * Biliomi
  */
 public class CallbackResources {
-  public static final String REDIRECT_URI = "http://127.0.0.1:23522/oauth/authorize.html";
+  public static final String REDIRECT_URI = "http://127.0.0.1:23522/oauth/authorize";
   public static final String HOST = "127.0.0.1";
   public static final int PORT = 23522;
 
-  public static URL getAuthPageFile() {
-    return CallbackResources.class.getResource("/oauth/authorize.html");
-  }
-
   public static URL getAuthFailedPageFile() {
-    return CallbackResources.class.getResource("/oauth/authorization-failure.html");
+    return CallbackResources.class.getResource("/oauth/auth-failed.html");
   }
 
   public static URL getAuthSuccessPageFile() {
-    return CallbackResources.class.getResource("/oauth/authorization-success.html");
+    return CallbackResources.class.getResource("/oauth/auth-success.html");
   }
 }
