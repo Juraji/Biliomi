015614a385debd0d07e5150ab3aabea39a77c43c
Juraji <robin@juraji.nl>
2017-12-18 00:31:11 +0100

    - ChannelSettingsComponent.java: Fix communities update stating no communities or object references

--- core/src/main/java/nl/juraji/biliomi/components/system/channel/ChannelSettingsComponent.java
+++ core/src/main/java/nl/juraji/biliomi/components/system/channel/ChannelSettingsComponent.java
@@ -18,6 +18,7 @@ import org.joda.time.DateTime;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Created by Juraji on 13-5-2017.
@@ -157,7 +158,10 @@ public class ChannelSettingsComponent extends Component {
         boolean communitiesUpdated = communitiesService.updateTwitchCommunities(communities);
         if (communitiesUpdated) {
           chat.whisper(user, i18n.get("ChatCommand.channel.game.communities.updated")
-              .add("communities", updateGameResult::getCommunities));
+              .add("communities", communities.stream()
+                  .limit(3)
+                  .map(Community::getName)
+                  .collect(Collectors.toList())));
         } else {
           chat.whisper(user, i18n.get("ChatCommand.channel.game.communities.updatedFailed"));
           return false;
