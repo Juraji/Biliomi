3f73ff2a9ad7a00d5b069133d7d0c27805ff56f0
Juraji <robin@juraji.nl>
2017-12-22 07:26:37 +0100

    - UserRestService.java: Allow updating follower and subscriber dates

--- core/src/main/java/nl/juraji/biliomi/rest/services/rest/core/UserRestService.java
+++ core/src/main/java/nl/juraji/biliomi/rest/services/rest/core/UserRestService.java
@@ -49,19 +49,18 @@ public class UserRestService extends ModelRestService<User> {
     }
 
     // Only some of the properties can be changed
-    user.setFollower(e.isFollower());
-    if (user.isFollower()) {
-      user.setFollowDate(e.getFollowDate());
-    }
-    user.setFollower(e.isSubscriber());
-    if (user.isSubscriber()) {
-      user.setFollowDate(e.getSubscribeDate());
-    }
-
     user.setUserGroup(e.getUserGroup());
     user.setTitle(e.getTitle());
     user.setPoints(e.getPoints());
     user.setBlacklistedSince(e.getBlacklistedSince());
+
+    // These can only be changed if
+    if (user.isFollower()) {
+      user.setFollowDate(e.getFollowDate());
+    }
+    if (user.isSubscriber()) {
+      user.setFollowDate(e.getSubscribeDate());
+    }
 
     userDao.save(user);
 
